f9be61e276194b42db12c5655f25ce16a2945474
Shannon <shannon.nell92@gmail.com>
2021-04-30 11:17:20 -0400

    start and end game functions

--- assets/js/stormlight.js
+++ assets/js/stormlight.js
@@ -10,14 +10,28 @@ var enemyAttack = 12;
 
 
 /* GAME FUNCTIONS */
+//window.alert("Would you like to destroy some evil today?");
 
 // fight function
 var fight = function(enemyName) {
-    window.alert("Would you like to destroy some evil today?");
+    // repeat and execute while enemy is alive
+    while(radiantHealth > 0 && enemyHealth > 0) {
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
 
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
-    // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // If player skips, confirm and stop loop    
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            // confirm skip
+            var confirmSkip = window.confirm("Are you sure your Order wants to leave the battle?");
+            
+            // if yes, leave fight
+            if (confirmSkip) {
+                window.alert("The " + radiantName + "'s have decided to skip the battle.");
+                //subtract spheres from radiantMoney for skipping
+                radiantSpheres = radiantSpheres - 10;
+                console.log("radiantSpheres", radiantSpheres);
+                break;
+            }
+        }
 
         // Remove enemy health; subtract radiantAttack from enemyHealth and log
         enemyHealth = enemyHealth - radiantAttack;
@@ -28,41 +42,25 @@ var fight = function(enemyName) {
         // Check enemy's health
         if (enemyHealth <=0) {
             window.alert(enemyName + " has been defeated.");
-        }
-        else {
+            break;
+        } else {
             window.alert(enemyName + " still has " + enemyHealth + " Voidlight remaining.");
         }
         
         // Remove player health; subtract enemyAttack from radiantHealth and log
         radiantHealth = radiantHealth - enemyAttack;
         console.log(
-            enemyName + " attacked " + radiantName + ". " + radiantName + " now has " + radiantHealth + " health remaining."
+            enemyName + " attacked " + radiantName + ". " + radiantName + " now have " + radiantHealth + " Stomlight remaining."
         );
         
         // Check radiant's health
         if (radiantHealth <=0) {
             window.alert(radiantName + " has run out of Stormlight.");
+            // leave while() loop if radiant ran out of Stormlight
+            break;
+        } else {
+            window.alert("The " + radiantName + "'s still have " + radiantHealth + " Stormlight remaining.");
         }
-        else {
-            window.alert(radiantName + " still has " + radiantHealth + " Stormlight left.");
-        }
-    // If player skips    
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm skip
-        var confirmSkip = window.confirm("Are you sure your Order wants to leave the fight?");
-        
-        // if yes, leave fight
-        if (confirmSkip) {
-            window.alert("The " + radiantName + "'s have decided to skip the fight. Goodbye.");
-            //subtract spheres from radiantMoney for skipping
-            radiantSpheres = radiantSpheres - 2;
-        }
-        //if no, ask question again by running fight() again
-        else {
-            fight();
-        }
-    } else {
-        window.alert("You need to choose a valid option. Try again.");
     }
 }
 
@@ -72,7 +70,52 @@ var fight = function(enemyName) {
 /* VARIABLES */
 
 
+//function to start a new game
+var startGame = function() {
+    //reset radiant stats
+    radiantHealth = 100;
+    radiantAttack = 10;
+    radiantSpheres = 10;
+
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (radiantHealth > 0) {
+            //let player know what floor they are on
+            window.alert("The battle for Uruthiru has begun! On floor " + ( i + 1 ) );
+
+            //pick new enemy to fight based on index of enemyNames
+            var pickedEnemyName = enemyNames[i];
+
+            //reset enemyHealth before starting a new fight
+            enemyHealth = 50;
+
+            fight(pickedEnemyName);
+        } else {
+            window.alert("Your Radiants have run out of Stormlight! Go ask Dalinar to open the perpindicularity and refill your Stormlight.");
+            break;
+        }
+    }
+    // after loop ends, player is either out of health or has no more enemies to fight, so run endGame function
+    endGame();
+};
+
+var endGame = function() {
+    // if player is still alive, they win!
+    if (radiantHealth > 0) {
+        window.alert("The battle is won, you've survived! You now have a score of " + radiantSpheres + ".");
+    }
+    else {
+        window.alert("Your Radiants ran out of Stormlight and have died.");
+    }
+
+    var playAgainConfirm = window.confirm ("Would you like to battle again?");
+    if (playAgainConfirm) {
+        //restart game
+        startGame();
+    }
+    else {
+        window.alert("Thank for your saving Uruthiru! Come back soon, there's always something going on.");
+    }
+};
+
 /* RUN GAME */
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-}
+startGame();
